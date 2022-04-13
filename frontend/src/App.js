2ac3f7808168ced8187a60f01a6b789388dd51c0
devinbi <it20062088@my.sliit.lk>
2022-04-13 23:22:52 +0530

    new

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Addexpense from './components/Addexpense.js';
+import Addincome from './components/Addincome.js';
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='App'>
+      
+      <Addincome/>
+      <Addexpense/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
