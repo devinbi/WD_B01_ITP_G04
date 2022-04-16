c8f4023a8807b17ba1fd8e64c6cfdecc6da55de4
isura1234 <maxiissa99@gmail.com>
2022-04-16 17:55:34 +0530

    remove bodypraser

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,7 +1,6 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
@@ -12,7 +11,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8070;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 const URL = process.env.MONGODB_URL;
 
