a1fdbd082be3b581bbc7b7c9f17358583d8c385a
UdanaIsiwari <issiwari@gmail.com>
2022-04-20 02:06:49 +0530

    update and delete raw materials

--- frontend/src/components/inventoryManagement/AllRawMaterials.js
+++ frontend/src/components/inventoryManagement/AllRawMaterials.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import MaterialTable from 'material-table';
+import { Modal } from "react-bootstrap";
+import UpdateRawMaterial from "./UpdateRawMaterial";
 
 
 export default function AllRawMaterials(props){
     const [RawMaterials, setRawMaterials] = useState([]);
     
+    const [StateUpdate, setStateUpdate] = useState(false)
+    const [RawMaterialUpdate, setRawMaterialUpdate] = useState()
+
+    const [StateDelete, setStateDelete] = useState(false)
+    const [RawMaterialDelete, setRawMaterialDelete] = useState()
+
+
     useEffect(()=>{
         
         
@@ -19,30 +28,84 @@ export default function AllRawMaterials(props){
             })
         
     },[])
+
+    function onDelete() {
+        axios.delete( "http://localhost:8070/RawMaterial/delete/"+ RawMaterialDelete)
+            .then((res) => {
+                console.log(res)
+                alert('Raw Material Details Deleted')
+                window.location.reload(true)//reload page
+
+            }).catch(() => {
+                alert('error while deleting raw material detail')
+            })
+
+    }
     console.log(RawMaterials.data);
+
     return(
-        <div class="container-fluid">
-        <MaterialTable  style={{background:"#E3ECFF"}}
-                    title="All Product Item Details"
+        <div>
 
-                    columns={[
+            <div class="container-fluid">
+            <MaterialTable  style={{background:"#E3ECFF"}}
+                        title="All Raw Material Details"
+
+                        columns={[
+                            
+                            { title: "Item Id", field: "ItemId", type: "string" },
+                            { title: "Item Name", field: "ItemName", type: "string" },
+                            { title: "Quantity", field: "Quantity", type: "string" },
+                            { title: "Item Type", field: "ItemType", type: "string" },
+
+                        ]}
+
+                        data={RawMaterials}
+                        options={{
+
+                            sorting: true,
+                            search:false,
+                            filtering : true,
+                            actionsColumnIndex: -1
+
+                        }}
+
+                        actions={[
+                            {
+                                icon: () => <button class="btn btn-sm btn-outline-warning">Update</button>,
+                                onClick: (event, rowData) => {
+                                    setRawMaterialUpdate(rowData); //setTransportDetailswithID
+                                    setStateUpdate(true); //setStatetrue
+                                }
+                            },
+                            {
+                                icon: () => <button class="btn btn-sm btn-outline-danger">Delete</button>,
+                                onClick: (event, rowData) => {
+                                    setRawMaterialDelete(rowData._id) //setidto delete
+                                    setStateDelete(true);   //setstatetrue
+                                }
+                            },
+                            
+                        ]}
+
+            />
+
+             {/* update modal */}
+             <Modal show={StateUpdate}>
+                    <Modal.Body>
+                        <UpdateRawMaterial data={RawMaterialUpdate} cl={() => setStateUpdate(false)} />
+                    </Modal.Body>
+                </Modal>
                         
-                        { title: "Item Id", field: "ItemId", type: "string" },
-                        { title: "Item Name", field: "ItemName", type: "string" },
-                        { title: "Quantity", field: "Quantity", type: "string" },
-                        { title: "Item Type", field: "ItemType", type: "string" },
+                {/* delete modal */}
+                <Modal show={StateDelete}>
+                    <Modal.Body>
+                        <p>You Want to delete this Product Item details ?</p>
+                        <button type="button" class="btn btn-outline-danger mr-3 pl-3" onClick={onDelete}>Delete</button>
+                        <button type="button" class="btn btn-outline-secondary pl-3" onClick={() => setStateDelete(false)}>Cancel</button>
+                    </Modal.Body>
+                </Modal>
 
-                    ]}
-
-                    data={RawMaterials}
-                    options={{
-                        sorting: true,
-                        search:false,
-                        paging :false,
-                        filtering : true
-                    }}
-
-        />
+            </div>
         </div>
     )
 }
---
+++ frontend/src/components/inventoryManagement/UpdateRawMaterial.js
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const Host = "http://localhost:8070/RawMaterial";
+
+
+export default function UpdateRawMaterial({ data, cl }){
+   
+    const [ItemId, setItemId] = useState("");
+    const [ItemName, setItemName] = useState("");
+    const [Quantity, setQuantity] = useState("");
+    const [ItemType, setItemType] = useState("");
+   
+    useEffect(()=>{
+        
+        setItemId(data.ItemId)
+        setItemName(data.ItemName)
+        setQuantity(data.Quantity)
+        setItemType(data.ItemType)
+        
+    },[])
+  
+  function sendData(e){
+    
+    e.preventDefault();
+          
+        const updateRawMaterial = {
+            ItemId,
+            ItemName,
+            Quantity,
+            ItemType,
+          }
+          
+
+    axios.put(`${Host}/update/${data._id}`,updateRawMaterial)
+      .then((response) => {
+        alert("Raw Material Details Updated Successfully!");
+        window.location.reload(true);
+      })
+      .catch((err) => {
+          console.log(err)
+        alert("Error occured !");
+      });
+    
+  };
+
+return (
+    <div class="area">
+      <div className="container">
+        <form onSubmit={sendData}>
+
+
+          <div className="mb-3">
+            <label for="ItemId" className="form-label">
+                Item Id :
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                name="ItemId"
+                onChange={(e)=>{
+                setItemId(e.target.value);
+            }}
+                value={ItemId}
+                disabled
+            />
+          </div>
+
+
+          <div className="mb-3">
+            <label for="ItemName" className="form-label">
+                Item Name:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                name="ItemName"
+                onChange={(e)=>{
+                setItemName(e.target.value);
+            }}
+                value={ItemName}>
+            </input>
+          </div>
+
+
+          <div className="mb-3">
+            <label for="Quantity" className="form-label">
+                Quantity
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                name="Quantity"
+                onChange={(e)=>{
+                setQuantity(e.target.value);
+            }}
+                value={Quantity}>
+            </input>
+          </div>
+         
+
+          <div className="mb-3">
+          <label for="ItemType">Item Type</label>
+
+          <select name="ItemType" id="ItemType"  
+            onChange={(e)=>{
+
+            setItemType(e.target.value);
+
+            }}
+
+            value={ItemType} > 
+
+                <option selected>Select Item Type</option>
+                <option value="Fabric">Fabric</option>
+                <option value="Trims">Trims</option>
+                <option value="Accessories">Accessories</option>
+                <option value="Fabric Paints">Fabric Paints</option>
+            </select>
+            </div>
+
+
+          <button
+            type="submit"
+            className="btn btn-success"
+            style={{ margin: "5px" }}>
+                Update
+          </button>
+
+          <button type="button" class="btn btn-primary " onClick={cl}>
+                Cancel
+          </button>
+
+        </form>
+      </div>
+    </div>
+  );
+
+
+}
