e3bc63bc3ca74a35cf095bd28efa9b4839d4de68
Dimuth-J <dimuthdimuth9@gmail.com>
2022-04-19 21:27:11 +0530

    Add item function done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/supplierManagement/Header';
 import AddItem from "./components/supplierManagement/AddItem";
 import AllItemDetails from './components/supplierManagement/AllItemDetails';
+import AllSplireDetails from './components/supplierManagement/AllSplireDetails';
 
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
@@ -14,7 +15,8 @@ function App() {
         <Header/>
 
         <Route path="/add" exact component={ AddItem } />
-        <Route path="/view" exact component={ AllItemDetails } />
+        <Route path="/viewI" exact component={ AllItemDetails } />
+        <Route path="/viewS" exact component={ AllSplireDetails } />
         
       </div>
     </Router>
