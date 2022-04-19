e612f5228e5d9cb9418fa87bff2bdbd6ae064f6d
ThinithiN <nimthara12@gmail.com>
2022-04-19 07:11:20 +0530

    Dressmaking Maintainance Technical fetch fuctions done

--- frontend/src/components/equipmentManagement/AllTechnicals.js
+++ frontend/src/components/equipmentManagement/AllTechnicals.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import MaterialTable from 'material-table';
-
+//a
 
 export default function AllTechnicals(props){
     const [alltechnical, setTechnical] = useState([]);
