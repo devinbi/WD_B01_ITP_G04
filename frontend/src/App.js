17619f5e636f73bf1115be3a58b8f02681a65ec2
LalithyaMaddumage <sandeepanilalithya155@gmail.com>
2022-05-21 16:10:25 +0530

    Final commit to branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,11 @@ import UpdateTransport from './components/transportManagement/UpdateTransport';
 import UpdateVehicles from './components/transportManagement/UpdateVehicles';
 import AddMaintenance from './components/transportManagement/AddMaintenance';
 import AllMaintenance from './components/transportManagement/AllMaintenance';
-import Report from './components/transportManagement/Report';
+import EReport from './components/transportManagement/Report';
 import TopHeader from './components/TopHeader';
 import Drivers from './components/transportManagement/Drivers';
 import OrderDes from './components/transportManagement/addTransportByOrder'
+import Dashboard from './components/Dashboard';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
@@ -22,17 +23,18 @@ function App() {
     <Router>
     <div>
     <TopHeader/>
-        <Route  path="/add" exact component={AddTransport}/>
-        <Route path= "/view" exact component={AllTransport}/>
+        <Route  path="/Tadd" exact component={AddTransport}/>
+        <Route path= "/Tview" exact component={AllTransport}/>
         <Route path= "/vadd" exact component={AddVehicle}/>
         <Route path= "/vview" exact component={AllVehicles}/>
         <Route path= "/update" exact component={UpdateTransport}/>
         <Route path= "/vupdate" exact component={UpdateVehicles}/>
         <Route path= "/madd" exact component={AddMaintenance}/>
         <Route path= "/mview" exact component={AllMaintenance}/>
-        <Route path= "/rview" exact component={Report}/>
+        <Route path= "/rview" exact component={EReport}/>
         <Route path= "/viewD" exact component={Drivers}/>
         <Route path= "/viewO" exact component={OrderDes}/>
+        <Route path= "/dash" exact component={Dashboard}/>
     </div>
     </Router>
   );
